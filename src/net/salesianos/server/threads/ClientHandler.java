c876af583937b56f8e8010a4b42d1280516db6f2
Dagaet <alojera15@gmail.com>
2024-01-08 18:10:11 +0000

    feat(ClientHandler): added client behaviour to print message

--- src/net/salesianos/server/threads/ClientHandler.java
+++ src/net/salesianos/server/threads/ClientHandler.java
@@ -1,5 +1,38 @@
 package net.salesianos.server.threads;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
 
-public class ClientHandler {
+import net.salesianos.shared.models.Message;
+
+public class ClientHandler extends Thread {
     
+    private Socket clientSocket;
+
+    public ClientHandler(Socket socket){
+        this.clientSocket = socket;
+    }
+
+    @Override 
+    public void run() {
+        String username = "";
+        try {
+            ObjectInputStream objInStream = new ObjectInputStream(this.clientSocket.getInputStream());
+
+            username = objInStream.readUTF();
+
+            while (true) {
+                Message msg = (Message) objInStream.readObject();
+                System.out.println(username + ": " + msg.getContent());
+            }
+
+        } catch (EOFException eofException) {
+            System.out.println("CERRANDO CONEXION CON " + username.toUpperCase());
+            // TODO: handle exception
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
