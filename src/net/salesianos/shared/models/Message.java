c8a1f49741992fe7d98c194c51304fa2c2b70fb8
Dagaet <alojera15@gmail.com>
2024-01-08 18:16:01 +0000

    feat(Message): created constructor & access methods

--- src/net/salesianos/shared/models/Message.java
+++ src/net/salesianos/shared/models/Message.java
@@ -1,5 +1,37 @@
 package net.salesianos.shared.models;
 
 public class Message {
+    private String date;
+    private String username;
+    private String content;
 
+    public Message(String date, String username, String content) {
+        this.date = date;
+        this.username = username;
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
