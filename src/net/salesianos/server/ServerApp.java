385cab5bc8c62633c31b078345c53ac0fff5db4f
AleVallejoGarcia <alevalga@gmail.com>
2024-01-08 13:16:57 +0000

    feat(ServerApp): Created basic server

--- src/net/salesianos/server/ServerApp.java
+++ src/net/salesianos/server/ServerApp.java
@@ -1,5 +1,22 @@
 package net.salesianos.server;
 
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import net.salesianos.server.threads.ClientHandler;
+
 public class ServerApp {
-    
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(50000);
+
+        while (true) {
+            System.out.println("Esperando conexión...");
+            Socket clienSocket = serverSocket.accept();
+            System.out.println("CONEXIÓN ESTABLECIDA");
+            ClientHandler clientHandler = new ClientHandler(clienSocket);
+            clientHandler.start();
+
+        }
+    }
 }
