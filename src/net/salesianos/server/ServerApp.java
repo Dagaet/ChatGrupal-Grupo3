1c024fd8720b00b07d6a551b6471dd0f43794a23
AleVallejoGarcia <alevalga@gmail.com>
2024-01-10 13:20:09 +0000

    fix(ServerApp): Solved errors with creations of object inputs and outputs

--- src/net/salesianos/server/ServerApp.java
+++ src/net/salesianos/server/ServerApp.java
@@ -1,20 +1,29 @@
 package net.salesianos.server;
 
 import java.io.DataInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import net.salesianos.server.threads.ClientHandler;
 
 public class ServerApp {
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(55000);
+        ArrayList<ObjectOutputStream> connectedObjOutputStream = new ArrayList<>();
 
         while (true) {
             System.out.println("Esperando conexión...");
-            Socket clienSocket = serverSocket.accept();
+            Socket clientSocket = serverSocket.accept();
             System.out.println("CONEXIÓN ESTABLECIDA");
-            ClientHandler clientHandler = new ClientHandler(clienSocket);
+            ObjectOutputStream clientObjOutStream = new ObjectOutputStream(clientSocket.getOutputStream());
+            connectedObjOutputStream.add(clientObjOutStream);
+            
+            ObjectInputStream clientObjInStream = new ObjectInputStream(clientSocket.getInputStream());
+            
+            ClientHandler clientHandler = new ClientHandler(clientObjInStream, clientObjOutStream, connectedObjOutputStream);
             clientHandler.start();
 
         }
