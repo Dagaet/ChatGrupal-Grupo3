1982541470e85301abedadf07c997534c57ffe55
AleVallejoGarcia <alevalga@gmail.com>
2024-01-17 13:43:48 +0000

    fix(ServerListener): print of message received without condition

--- src/net/salesianos/client/threads/ServerListener.java
+++ src/net/salesianos/client/threads/ServerListener.java
@@ -5,28 +5,21 @@ import java.io.ObjectInputStream;
 
 import net.salesianos.shared.models.Message;
 
-public class ServerListener extends Thread{
+public class ServerListener extends Thread {
     private ObjectInputStream objInStream;
+
     public ServerListener(ObjectInputStream socketObjectInputStream) {
         this.objInStream = socketObjectInputStream;
     }
 
-    //hacer aqui lo de recibir mensajes de servidor
-    // ObjectInputStream clientObjInStream, ObjectOutputStream clientObjOutStream, 
     @Override
     public void run() {
         try {
             while (true) {
                 Message newMessage = (Message) this.objInStream.readObject();
-                if (newMessage.getUsername().equals("ServerOut")) {
-                    System.out.println(newMessage.getContent());
-                } else {
-                    System.out.println(newMessage.getUsername() + ": " + newMessage.getContent());
-                }
+                System.out.println(newMessage);
             }
-        } catch (ClassNotFoundException e1) {
-            System.out.println("No se encontró la clase message");
-        } catch (IOException e2) {
+        } catch (IOException | ClassNotFoundException e2) {
             System.out.println("Saliendo del chat...");
         }
     }
